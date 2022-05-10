d017360e41f6f29abe66bc34c715997dc17265d3
nuri <nuri@172.18.136.100>
2022-05-10 21:09:49 +0900

    created Dog.java

---
+++ app/src/main/java/edu/handong/csee/java/inheritance/Cat.java
@@ -0,0 +1,27 @@
+package edu.handong.csee.java.inheritance;
+
+public class Cat extends Animal {
+    public static void testClassMethod() {
+        System.out.println("The static method in Cat");
+    }
+    public void testInstanceMethod() {
+        System.out.println("The instance method in Cat");
+    }
+
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+        Animal myAnimal = myCat; //polymorphism
+        Animal.testClassMethod(); //hiding
+        myAnimal.testInstanceMethod(); //overriding
+        
+        Dog myDog = new Dog();
+        Animal myAnimald = myDog; //polymorphism
+        Animal.testClassMethod(); //hiding
+        myAnimald.testInstanceMethod(); //overriding
+        
+        Monkey myMonkey = new Monkey();
+        Animal myAnimalm = myMonkey; //polymorphism 
+        Animal.testClassMethod(); //hiding
+        myAnimalm.testInstanceMethod(); //overriding
+    }
+}
